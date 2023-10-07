d1279b609302fcdf89df760d3c4c2fa6ed3da187
sor4chi <sorakawa0218@gmail.com>
2023-10-07 18:44:13 +0900

    fix: base layout height height

--- src/components/layouts/Base.css.ts
+++ src/components/layouts/Base.css.ts
@@ -1,9 +1,12 @@
 import { style } from "@vanilla-extract/css";
 
+const TOP_PADDING = "10dvh";
+const BOTTOM_PADDING = "1rem";
+
 export const styles = {
   container: style({
-    minHeight: "100vh",
+    minHeight: `calc(100dvh - ${TOP_PADDING} - ${BOTTOM_PADDING})`,
     margin: "0 auto",
-    padding: "10vh 1rem 1rem",
+    padding: `${TOP_PADDING} 1rem ${BOTTOM_PADDING}`,
   }),
 };
