0bf7d0fade25ef919e4be3b5abb2784124b330a3
sor4chi <sorakawa0218@gmail.com>
2023-10-07 18:37:57 +0900

    feat: implement base layout

---
+++ src/components/layouts/Base.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+export const styles = {
+  container: style({
+    minHeight: "100vh",
+    margin: "0 auto",
+    padding: "10vh 1rem 1rem",
+  }),
+};
