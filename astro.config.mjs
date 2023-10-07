8a8c219733fd48af2c7267473f11c1482135d95a
sor4chi <sorakawa0218@gmail.com>
2023-10-07 21:59:18 +0900

    feat: support math syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [vanillaExtractPlugin()],
+  },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
