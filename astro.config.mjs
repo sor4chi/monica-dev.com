c48ad513c7d7df08d86f7e77e56da5a079b993ee
sor4chi <sorakawa0218@gmail.com>
2023-10-20 23:41:07 +0900

    feat: add react and mdx integration deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,19 @@ import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
 import rehypeHeadLinker from "./src/libs/rehype-head-linker";
 import rehypeAnnotationBlock from "./src/libs/rehype-annotation-block";
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [vanillaExtractPlugin()],
+    plugins: [vanillaExtractPlugin()]
   },
   markdown: {
     syntaxHighlight: "prism",
     remarkPlugins: [remarkMath, remarkBreaks],
-    rehypePlugins: [rehypeKatex, rehypeSlug, rehypeHeadLinker, rehypeAnnotationBlock],
+    rehypePlugins: [rehypeKatex, rehypeSlug, rehypeHeadLinker, rehypeAnnotationBlock]
   },
+  integrations: [mdx(), react()]
 });
