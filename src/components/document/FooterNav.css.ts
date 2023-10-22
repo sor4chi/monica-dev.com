c9f0836ff6fc47352116489a3c6e33af407ab154
sor4chi <sorakawa0218@gmail.com>
2023-10-23 00:24:57 +0900

    feat: add footer nav

---
+++ src/components/document/FooterNav.css.ts
@@ -0,0 +1,12 @@
+import { style } from "@vanilla-extract/css";
+
+export const styles = {
+  container: style({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    margin: "1rem auto",
+    fontSize: "0.875rem",
+  }),
+};
