fcf151923fb4687e4eec90e29358309285be0772
sor4chi <sorakawa0218@gmail.com>
2023-11-10 00:26:51 +0900

    feat: add feed

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = "Monica's Portfolio";
+export const SITE_DESCRIPTION =
+  "Webエンジニア Monica のポートフォリオサイトです。経歴やスキル、制作物などを掲載しています。";
+export const SITE_BASE_URL = "https://monica-dev.com";
---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,21 @@
+import { SITE_BASE_URL, SITE_DESCRIPTION, SITE_NAME } from "@/config";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET() {
+  const blogs = await getCollection("originalBlogs");
+
+  return rss({
+    title: SITE_NAME,
+    description: SITE_DESCRIPTION,
+    site: SITE_BASE_URL,
+    items: blogs.map(({ slug, data }) => ({
+      title: data.title,
+      description: data.description,
+      date: data.publishedAt,
+      link: `${SITE_BASE_URL}/blog/${slug}`,
+      pubDate: data.publishedAt,
+    })),
+    customData: `<language>ja</language>`,
+  });
+}
