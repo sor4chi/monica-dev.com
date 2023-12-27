8a91834483ef2c8ed37f1c8a6009a235bd869d0c
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-12-27 09:56:04 +0900

    feat: add timezone option to RelativeDate

--- src/components/mdx/RelativeDate.tsx
+++ src/components/mdx/RelativeDate.tsx
@@ -2,13 +2,17 @@ interface RelativeDateProps {
   date: string | Date;
   format: "year" | "month" | "day";
   add?: number;
+  timeZone?: string;
 }
 
-export const RelativeDate = ({ date, format, add = 0 }: RelativeDateProps) => {
+export const RelativeDate = ({ date, format, add = 0, timeZone = "Asia/Tokyo" }: RelativeDateProps) => {
   const now = new Date();
   const actualDate = new Date(date);
 
-  const diff = Math.abs(now.getTime() - actualDate.getTime());
+  const nowInTimeZone = new Date(now.toLocaleString("en-US", { timeZone }));
+  const actualDateInTimeZone = new Date(actualDate.toLocaleString("en-US", { timeZone }));
+
+  const diff = Math.abs(nowInTimeZone.getTime() - actualDateInTimeZone.getTime());
   const diffDays = Math.floor(diff / (1000 * 3600 * 24));
   const diffMonths = Math.floor(diffDays / 30);
   const diffYears = Math.floor(diffMonths / 12);
