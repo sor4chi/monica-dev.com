bae1e37cb62ed91c69cff2d8acbc614df3924dc0
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-03-27 20:30:00 +0900

    feat: make more readable by expanding article width

--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -2,7 +2,7 @@ import { sumOfRems } from "@/styles/utils";
 
 export const CONTAINER_GAP = "1.5rem";
 export const ASIDE_WIDTH = "15rem";
-export const ARTICLE_WIDTH = "40rem";
+export const ARTICLE_WIDTH = "50rem";
 export const LAYOUT_CONTAINER_SIDE_PADDING = "1rem";
 export const NAVIGATION_AREA_WIDTH = "20rem";
 
