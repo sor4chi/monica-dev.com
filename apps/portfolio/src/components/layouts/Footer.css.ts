03aa9bd531584bc0930803eb5d4346bbd746625f
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-05-24 23:25:26 +0900

    feat: add footer

---
+++ apps/portfolio/src/components/layouts/Footer.css.ts
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from 'design-system/theme.css';
+
+export const styles = {
+  footer: style({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: vars.spacing.absolute[2],
+    fontSize: vars.font.size.sm,
+    padding: `${vars.spacing.relative[4]} 0`,
+    marginTop: vars.spacing.absolute[16],
+  }),
+  footerText: style({
+    color: vars.color.gray[10],
+  }),
+};
