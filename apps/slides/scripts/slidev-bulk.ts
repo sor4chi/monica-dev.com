06c0a844e9e23ee83a55fce34eb40b8b912516ac
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-06-21 20:45:51 +0900

    feat: add slides app

---
+++ apps/slides/scripts/slidev-bulk.ts
@@ -0,0 +1,16 @@
+import { exec } from "node:child_process";
+import { parseArgs } from "node:util";
+
+import glob from "fast-glob";
+
+const files = await glob("slides/*.md");
+const pwd = process.cwd();
+
+const BUILD = (file: string, basename: string) =>
+  `slidev build ${file} --out "${pwd}/dist/${basename}" --base "${basename}"`;
+
+for (const file of files) {
+  const res = exec(BUILD(file, file.replace(/.*\/(?<name>.*)\.md/, "$<name>")));
+  res.stdout?.pipe(process.stdout);
+  res.stderr?.pipe(process.stderr);
+}
